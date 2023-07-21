58f9f1a9c986161a5ba05ef393860c2ec9b89956
Hariharanj10 <hariharan03102001@gmail.com>
2023-07-21 17:03:55 +0530

    21-7-23

--- React-ui/myapp/src/ProfileComponent.js
+++ React-ui/myapp/src/ProfileComponent.js
@@ -1,51 +1,79 @@
 import React from "react";
 import styled from "styled-components";
-import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+import { FaStar, FaStarHalf } from "react-icons/fa";
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-center: center;
+  flex-wrap: wrap;
+`;
 const ProfileCardContainer = styled.div`
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin-bottom: 10px;
+  display: flex;
+  gap: 30px;
+  width: 40%;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 20px;
+  padding: 20px;
+  box-shadow: 2px 2px 2px 2px #999;
 `;
-
+const StyledItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+  justify-content: center;
+`;
 const ProfileImage = styled.img`
-  max-width: 100px;
-  max-height: 100px;
+  max-width: 200px;
+  max-height: 300px;
 `;
-const StyledStar=styled.div`
-
-`
-const ProfileComponent = ({ props ,onDelete,stars}) => {
-    console.log(stars)
-    const getStars = (props) => {
-        const starCount= [];
-        console.log(props)
-        const floorCount= Math.round(props * 2) / 2;
-        for (let index= 1; index <= 5; index++) {
-          if (floorCount >= index) {
-            starCount.push(<FaStar key={index} color='#B59410' size="30px"/>);
-          } else if (floorCount >= index - 0.5) {
-            starCount.push(<FaStarHalfAlt key={index} color='#B59410' size="30px"/>);
-          } else {
-            starCount.push(<FaRegStar key={index} color='#B59410' size="30px"/>);
-          }
-        }
-        return starCount;
-      };
+const StyledButton = styled.button`
+  border: none;
+  background-color: wheat;
+  width: 80px;
+  height: 30px;
+  text-transform: uppercase;
+  font-weight: bold;
+  border-radius: 20px;
+`;
+const StyledStar = styled.div``;
+const ProfileComponent = ({ props, onDelete}) => {
+  const getStars = (rating) => {
+    const stars = [];
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(<FaStar key={i} color="yellow" size={25}></FaStar>);
+    }
+    if (hasHalfStar) {
+      stars.push(<FaStarHalf key="half" color="yellow" size={25}></FaStarHalf>);
+    }
+    while (stars.length < 5) {
+      stars.push(<FaStar key={stars.length} color="white" size={25}></FaStar>);
+    }
+    return stars;
+  };
   return (
-    <div>
-      {props.map((profileData, index) => (
+    <StyledContainer>
+      {props?.map((profileData, index) => (
         <ProfileCardContainer key={index}>
-          <h2>Name: {profileData.username}</h2>
-          <p>Location: {profileData.location}</p>
-          <p>Rating: {profileData.rating}</p>
-          <starsArray rating={parseFloat(profileData.rating)} />
-          <div>{getStars(profileData.rating)}</div>
-          <p>Feedback: {profileData.feedback}</p>
-          <ProfileImage src={profileData.img} alt="Profile" />
-         <button onClick={() => onDelete(index)}>Delete</button>
+          <StyledItem>
+            <ProfileImage src={profileData.img} alt="Profile" />
+          </StyledItem>
+          <StyledItem>
+            <p>Name: {profileData.username}</p>
+            <p>Location: {profileData.location}</p>
+            <p>Rating: {profileData.rating}</p>
+            <StyledStar rating={parseFloat(profileData.rating)} />
+            <div>{getStars(profileData.rating)}</div>
+            <p>Feedback: {profileData.feedback}</p>
+
+            <StyledButton onClick={() => onDelete(index)}>Delete</StyledButton>
+          </StyledItem>
         </ProfileCardContainer>
       ))}
-    </div>
+    </StyledContainer>
   );
 };
 
--- React-ui/myapp/src/Test.js
+++ React-ui/myapp/src/Test.js
@@ -1,30 +1,67 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import ProfileComponent from "./ProfileComponent";
-import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+const StyledMainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #e9f1fb;
+`;
 const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const StyledForm = styled.form`
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    width:100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  font-weight: bold;
+  width: 50%;
+  background-color: #f2f6fb;
+  margin-top: 10px;
+  padding: 20px;
 `;
-    
+
 const StyledItem = styled.div`
-  display: block;
-  margin:2px;
-  padding:2px;
+  margin: 2px;
+  padding: 2px;
+  display: flex;
+  flex-direction: column;
 `;
-
-const DropdownContainer = styled.select``;
-const RatingContainer = styled.select``;
-
-const imgSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTqgP7B9wb6Kb3qhPlyzSBUDp4fER7sdOydG1PLCz-8x34wgU0p36SKOKyYX17M0bHag4&usqp=CAU"; 
+const StyledInput = styled.input`
+  width: 200px;
+  height: 30px;
+  border: 2px solid #000000;
+`;
+const StyledButton = styled.button`
+  border: none;
+  background-color: wheat;
+  width: 80px;
+  height: 30px;
+  text-transform: uppercase;
+  font-weight: bold;
+`;
+const DropdownContainer = styled.select`
+  width: 200px;
+  height: 30px;
+  border: 2px solid #000000;
+`;
+const RatingContainer = styled.select`
+  width: 200px;
+  height: 30px;
+  border: 2px solid #000000;
+`;
+const StyledText = styled.textarea`
+  width: 200px;
+  height: 60px;
+  border: 2px solid #000000;
+`;
+const imgSrc =
+  "https://www.pngkey.com/png/full/22-223848_businessman-vector-person-logo-png.png";
 
 const FormComponent = () => {
   const [formData, setFormData] = useState({
@@ -34,13 +71,14 @@ const FormComponent = () => {
     feedback: "",
     img: imgSrc,
   });
-  const [final, setFinal] = useState([]); 
+  const [final, setFinal] = useState([]);
 
   const [user, setUserName] = useState("");
   const [ln, setLocation] = useState("");
   const [ratingGiven, setRatingGiven] = useState("");
   const [text, setText] = useState("");
-
+  const [userFilter, setUserFilter] = useState([]);
+  const [filterValue, setFilterValue] = useState("");
   const locationOptions = ["chennai", "Bangalore", "Coimbatore"];
   const ratingOptions = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
 
@@ -53,7 +91,7 @@ const FormComponent = () => {
       feedback: text,
       img: imgSrc,
     };
-    setFormData(newFormData); 
+    setFormData(newFormData);
     setFinal((prevFinal) => [...prevFinal, newFormData]);
     setUserName("");
     setLocation("");
@@ -63,26 +101,34 @@ const FormComponent = () => {
   const handleDelete = (index) => {
     setFinal((prevFinal) => prevFinal.filter((k, i) => i !== index));
   };
-
+  const handleSearch = (e) => {
+    // setFilterValue(e.target.value)
+    // console.log(filterValue)
+    setUserFilter(
+      final.filter((item) => item.username.startsWith(e.target.value))
+    );
+  };
   return (
-    <StyledContainer>
-      <StyledForm onSubmit={handleSubmit}>
-        <StyledItem>
-          <label>
-            Name:
-            <input
+    <StyledMainContainer>
+      <StyledContainer>
+        <StyledForm onSubmit={handleSubmit}>
+          <h1>Feedback Form</h1>
+          <StyledItem>
+            <label>Name:</label>
+            <StyledInput
               type="text"
               name="name"
               value={user}
               onChange={(e) => setUserName(e.target.value)}
               required
             />
-          </label>
-        </StyledItem>
-        <StyledItem>
-          <label>
-            Location:
-            <DropdownContainer value={ln} onChange={(e) => setLocation(e.target.value)}>
+          </StyledItem>
+          <StyledItem>
+            <label>Location:</label>
+            <DropdownContainer
+              value={ln}
+              onChange={(e) => setLocation(e.target.value)}
+            >
               <option value="">Select an option</option>
               {locationOptions.map((option) => (
                 <option key={option} value={option}>
@@ -90,12 +136,13 @@ const FormComponent = () => {
                 </option>
               ))}
             </DropdownContainer>
-          </label>
-        </StyledItem>
-        <StyledItem>
-          <label>
-            Rating:
-            <RatingContainer value={ratingGiven} onChange={(e) => setRatingGiven(e.target.value)}>
+          </StyledItem>
+          <StyledItem>
+            <label>Rating:</label>
+            <RatingContainer
+              value={ratingGiven}
+              onChange={(e) => setRatingGiven(e.target.value)}
+            >
               <option value="">Select an option</option>
               {ratingOptions.map((option) => (
                 <option key={option} value={option}>
@@ -103,25 +150,28 @@ const FormComponent = () => {
                 </option>
               ))}
             </RatingContainer>
-          </label>
-        </StyledItem>
-        <StyledItem>
-            <label>
-            Review:
-          <textarea
-            name="postContent"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-          </label>
-        </StyledItem>
-        <StyledItem>
-          <button type="submit">Submit</button>
-        </StyledItem>
-      </StyledForm>
-      {formData.username && <ProfileComponent props={final} onDelete={handleDelete}  />}
-    </StyledContainer>
+          </StyledItem>
+          <StyledItem>
+            <label>Review:</label>
+            <StyledText
+              name="postContent"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </StyledItem>
+          <StyledItem>
+            {user && <StyledButton type="submit">Submit</StyledButton>}
+          </StyledItem>
+        </StyledForm>
+        <input onChange={handleSearch}></input>
+      </StyledContainer>
+      <ProfileComponent props={userFilter.length > 0 ? userFilter :final} onDelete={handleDelete} />;
+    </StyledMainContainer>
   );
 };
 
 export default FormComponent;
+
+//{ formData.username && (
+// <ProfileComponent props={final} onDelete={handleDelete} />
+// )}
